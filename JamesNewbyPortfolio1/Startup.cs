f5267a65e314a5f3fe2e9229dffd448d60d80bc8
jenewby54n <jenewby54n@yahoo.com>
2016-01-06 14:58:54 -0500

    Site 1 try
    first attempt at making a website on my own. will probably redo

---
+++ JamesNewbyPortfolio1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JamesNewbyPortfolio1.Startup))]
+namespace JamesNewbyPortfolio1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
